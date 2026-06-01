a594e4612d82f6ee06b3843b8328c02206c5086d
Ryan Yudhistira <113612935+rrdyudhistira@users.noreply.github.com>
2026-06-02 00:52:52 +0700

    Add files via upload

--- database.sql
+++ database.sql
@@ -1,17 +1,19 @@
-CREATE DATABASE IF NOT EXISTS db_tka2026;
-USE db_tka2026;
 
-CREATE TABLE siswa (
-id INT AUTO_INCREMENT PRIMARY KEY,
-nomor_peserta VARCHAR(30),
-nisn VARCHAR(20) UNIQUE,
-nama VARCHAR(150),
-ttl VARCHAR(150),
-matematika DECIMAL(5,2),
-bahasa_indonesia DECIMAL(5,2)
+CREATE DATABASE IF NOT EXISTS tka_db;
+USE tka_db;
+
+CREATE TABLE peserta_tka (
+    id INT PRIMARY KEY,
+    nomor_peserta VARCHAR(50),
+    kolom_2 VARCHAR(255),
+    nisn BIGINT,
+    nama VARCHAR(255),
+    kolom_5 VARCHAR(255),
+    tempat_tanggal_lahir VARCHAR(255),
+    kolom_7 VARCHAR(255),
+    nilai_literasi VARCHAR(50),
+    nilai_numerasi VARCHAR(50),
+    kolom_10 VARCHAR(255),
+    kolom_11 VARCHAR(255),
+    kolom_12 VARCHAR(255)
 );
-
-INSERT INTO siswa
-(nomor_peserta,nisn,nama,ttl,matematika,bahasa_indonesia)
-VALUES
-('T2-26-02-06-0007-0001-8','0117567689','AMIRAH SHOFIYAWATI','Cirebon, 8 Maret 2011',56.67,73.33);
